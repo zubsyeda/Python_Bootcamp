321a7056a2222d96b83f1072671b9d59317ac02f
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-07-11 11:47:57 -0500

    Mail Merge Project Done
    Used files and directories

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,9 +1,24 @@
+
+
 #TODO: Create a letter using starting_letter.txt
+letter = open("Input/Letters/starting_letter.txt")
+template = letter.read()
+letter.close()
 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-with open(./Input/Letters/"starting_letter.txt")
+names = open("Input/Names/invited_names.txt")
+namesList = names.readlines()
+names.close()
+
+for item in namesList:
+    name = item.strip()
+    file_name = "letter_for_" + name
+    file = open(f"Output/ReadyToSend/{file_name}", mode="w")
+    updated_letter = template.replace("[name]", name)
+    file.write(updated_letter)
+    file.close()
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
